f0ba2a13f529fe9afdedb5a4d9bbf4d5f4b09c93
smsaleemullah <160390431+smsaleemullah@users.noreply.github.com>
2024-03-28 20:11:32 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+
+// Initialize user balance and pin code
+let myBalance = 5000;
+let myPin = 1234;
+ 
+// Print welcome message
+console.log("Welcome to Maryam Saleem - ATM Machine");
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type:"number",
+        message: "Enter your pin code:"
+    }
+])
+if (pinAnswer.pin === myPin){
+    console.log("Pin is Correct, Login Successfully!");
+    //console.log(`Current Account Balance is ${myBalance}`)
+
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Select an operation:",
+            choices:["Withdraw Ammount", "Check Balance"]
+        }
+    ])
+
+    if(operationAns.operation === "Withdraw Ammount"){
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter the amount to withdraw"
+            }
+        ]) 
+        if(amountAns.amount > myBalance){
+            console.log("Insufficient Balance");
+        }
+        else{
+            myBalance -= amountAns.amount;
+            console.log(`${amountAns.amount} Withdraw Successfully`);
+            console.log(`Your Remaining Balance is: ${myBalance}`);
+        }
+    }
+    else if (operationAns.operation === "Check Balance"){
+        console.log(`Your Account Balance is: ${myBalance}`);
+    }
+}
+else{
+    console.log("Pin is Incorrect, Try Again!");
+}
